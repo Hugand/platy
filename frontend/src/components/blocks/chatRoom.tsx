e63743ffb47856eae08c5ece74ea3661090c6681
Hugo Gomes <hugoasgomes@hotmail.com>
2021-04-23 03:14:49 +0100

    Test eslint error 2

--- frontend/src/components/blocks/chatRoom.tsx
+++ frontend/src/components/blocks/chatRoom.tsx
@@ -13,7 +13,7 @@ import { ChatLoadingSkeleton } from '@skeletons/chatLoadingSkeleton';
 
 interface Props {
     friend: User;
-    setIsInRoom: any; //React.Dispatch<React.SetStateAction<boolean>>;
+    setIsInRoom: Function; //React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const ChatRoom: React.FC<Props> = ({ friend, setIsInRoom }) => {
